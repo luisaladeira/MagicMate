38ae9fde207a39e05e5361bc8cbd8914b97f31f1
luisaladeira <luisagalvaoladeira@gmail.com>
2023-12-14 23:36:42 -0300

    Inclusão de novos métodos

--- theme-parks/__init__.py
+++ theme-parks/__init__.py
@@ -3,6 +3,7 @@ from ThemeParks import ThemeParks
 
 def save_infos_about_all_destinations(ThemeParks : ThemeParks) -> None:
     destinations = ThemeParks.get_destinations()
+    print('Salvando Complexos -> ', destinations)
 
 def get_list_of_parks_in_locations(ThemeParks : ThemeParks, locations : list) -> list:
     list_parks = ThemeParks.get_parks_ids(locations)
@@ -10,10 +11,52 @@ def get_list_of_parks_in_locations(ThemeParks : ThemeParks, locations : list) ->
 
 def save_info_about_parks(ThemeParks : ThemeParks, park_list : list) -> None:
     infos = ThemeParks.get_entity_infos(park_list)
+    print('Salvando Infos de Parques -> ', infos)
 
 def save_info_about_locations(ThemeParks : ThemeParks, locations_list : list) -> None:
     infos = ThemeParks.get_entity_infos(locations_list)
+    print('Salvando Infos de Complexos -> ', infos)
 
+
+def get_atractions_of_parks(ThemeParks : ThemeParks, park_list : list) -> None:
+    park_atractions = []
+    atractions = []
+    for park_id in park_list:
+        print(f'Buscando dados de: {park_id}')
+        park_atractions = ThemeParks.get_entity_children(entity=park_id)
+    
+        for item in park_atractions['children']:
+            atractions.append(item['id'])
+    
+    
+    return atractions
+
+
+def save_atractions_of_parks(ThemeParks : ThemeParks, park_list : list) -> None:
+    atractions = get_atractions_of_parks(ThemeParks, park_list)
+    print('Salvando Atrações de cada Parque -> ', atractions)
+
+
+def save_info_about_atractions(ThemeParks : ThemeParks, atraction_list : list):
+    infos = ThemeParks.get_entity_infos(atraction_list)
+    print('Salvando Infos de atrações -> ', infos)
+
+
+def save_atraction_live(ThemeParks : ThemeParks, park_list : list):
+    live = []
+
+    for park in park_list:
+        print('Salvando Infos do parque -> ', park)
+        dict_live = {}
+        
+        dict_live['park_id'] = park
+        dict_live['live'] = ThemeParks.get_entity_live(entity=park)
+        live.append(dict_live)
+
+        return live
+    
+    print('Salvando Infos de Tempo de Espera dos Parques -> ', live)
+    
 
 
 def __main__():
@@ -26,4 +69,9 @@ def __main__():
 
     save_info_about_locations(tp, orlando_parks)
     save_info_about_parks(tp, park_list)
+    save_atractions_of_parks(tp, park_list)
+
+    atraction_list = get_atractions_of_parks(tp, park_list)
+    save_info_about_atractions(tp, atraction_list)
+    save_atraction_live(tp, park_list)
 
