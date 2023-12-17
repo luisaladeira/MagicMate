3abc3c34bc3ec712eb3738bc40238cb9d3169aa8
luisaladeira <luisagalvaoladeira@gmail.com>
2023-12-17 09:55:10 -0300

    Ajuste funstions

--- http-themepark/__init__.py
+++ http-themepark/__init__.py
@@ -1,10 +1,7 @@
 import azure.functions as func
 import logging
 
-app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
-
-@app.route(route="http_themeparks")
-def http_themeparks(req: func.HttpRequest) -> func.HttpResponse:
+def __main__(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     name = req.params.get('name')
